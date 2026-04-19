05062c4e0af56ae42a6bf1b2856a1ff7bfec5af3
Thirumurthi S <thirumurthi.s@gmail.com>
2026-04-18 22:07:03 -0700

    Update wsl_docker_ubuntu_26-04.sh

--- WSL2/docker/wsl_docker_ubuntu_26-04.sh
+++ WSL2/docker/wsl_docker_ubuntu_26-04.sh
@@ -25,3 +25,13 @@ sudo systemctl status docker
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
+
+###############
+### If docker pull error with below such message
+# ERROR: failed to create cluster: failed to pull image "kindest/node:v1.35.0@sha256:452d707d4862f52530247495d180205e029056831160e22870e37e3f6c1ac31f": command "docker pull kindest/node:v1.35.0@sha256:452d707d4862f52530247495d180205e029056831160e22870e37e3f6c1ac31f" failed with error: exit status 1
+# Command Output: docker-credential-secretservice: error while loading shared libraries: libsecret-1.so.0: cannot open shared object file: No such file or directory
+# error getting credentials - err: exit status 127, out: ``
+######### fix
+#  create a ~/.docker/config.json and add below content. use mkdir -p ~/.docker
+#   { "credsStore": "pass" }
+#
